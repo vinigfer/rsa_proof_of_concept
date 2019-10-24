a82df38bc76deb763b51ce3c2aa7b58f885775ca
Vinicius Gubiani Ferreira <vini.g.fer@gmail.com>
2019-10-24 00:44:37 -0300

    docs: Improve README.md on how to run
    Also some minor improvements to the code

--- README.md
+++ README.md
@@ -7,7 +7,27 @@ How to use this:
 python3.7
 from rsa import *
 rsa = RSA()
-rsa.trabalho()
+rsa.setup()
+msg_encriptada = rsa.encrypt("Mensagem ultra-secreta", rsa.public_key)
+rsa.decrypt(msg_encriptada, rsa.private_key)
+```
+
+Or you can run individual steps one by one, and see the created values:
+```
+python3.7
+from rsa import *
+rsa = RSA()
+
+p = find_prime_with_fermat()
+q = find_prime_with_fermat()
+
+rsa.set_p(p)
+rsa.set_q(q)
+rsa.calculate_n()
+rsa.calculate_phi()
+rsa.generate_e()
+rsa.calculate_d()
+
 msg_encriptada = rsa.encrypt("Mensagem ultra-secreta", rsa.public_key)
 rsa.decrypt(msg_encriptada, rsa.private_key)
 ```
--- rsa.py
+++ rsa.py
@@ -1,12 +1,18 @@
+"""
+Implementacao do algoritmo RSA em python3.7
+Autor: Vinicius Ferreira
+Data: 24/Outubro/2019
+Como usar: consulte o arquivo README.md
+"""
+
 from math import gcd
 from random import randint
 from sys import getrecursionlimit, setrecursionlimit
 
 
-def find_prime_with_fermat(number_of_bits=1000):
+def find_prime_with_fermat(number_of_bits=1024):
     MINIMUM = pow(2, number_of_bits)
     MAXIMUM = pow(2, number_of_bits + 1) - 1
-
     while True:
         p = randint(MINIMUM, MAXIMUM)
         if pow(2, p -1, p) == 1:
@@ -16,22 +22,24 @@ def find_prime_with_fermat(number_of_bits=1000):
 def egcd(a_value, b_value):
     if a_value == 0:
         return b_value, 0, 1
-
     g, x, y = egcd(b_value % a_value, a_value)
-
     return g, y - (b_value // a_value) * x, x
 
 
 def modinv(a, m):
     g, x, y = egcd(a, m)
-
     if g != 1:
-        raise Exception('Sem inversão modular')
-
+        raise Exception("No modular inversion")
     return x % m
 
 
 class RSA(object):
+    def __init__(self):
+        # Often we hit the maximum recursion stack for python (which is 1000) during egcd method.
+        # Here we stretch the limit a bit further, as from tests we only needed a few more iterations.
+        # print(getrecursionlimit())
+        setrecursionlimit(2000)
+
     @property
     def public_key(self):
         return self.n, self.e
@@ -40,22 +48,28 @@ class RSA(object):
     def private_key(self):
         return self.p, self.q, self.d
 
-    def trabalho(self):
-        # Need to increase max recursion for python stack
-        # print(getrecursionlimit())
-        setrecursionlimit(2000)
+    def setup(self):
+        """Run in a single batch all the necessary requirements to get RSA encryption working"""
 
+        # We can also call methods set_p and set_q manually if we already have the prime_numbers
         self.create_prime_numbers()
+
         self.calculate_n()
         self.calculate_phi()
         self.generate_e()
         self.calculate_d()
 
     def create_prime_numbers(self):
-        self.p = find_prime_with_fermat()
-        self.q = find_prime_with_fermat()
+        self.set_p(find_prime_with_fermat())
+        self.set_q(find_prime_with_fermat())
         while self.p == self.q:
-            self.q = find_prime_with_fermat()
+            self.set_q(find_prime_with_fermat())
+
+    def set_p(self, p):
+        self.p = p
+
+    def set_q(self, q):
+        self.q = q
 
     def calculate_n(self):
         self.n = self.p * self.q
@@ -65,9 +79,8 @@ class RSA(object):
 
     def generate_e(self):
         self.e = randint(1, self.phi)
-
         if self.e < 2 or gcd(self.e, self.phi) != 1:
-            self.gerar_e()
+            self.generate_e()
 
     def calculate_d(self):
         self.d = modinv(self.e, self.phi)
